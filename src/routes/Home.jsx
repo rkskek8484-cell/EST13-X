56547403f22ae5ebd39ce1bb1586da8bcc794cde
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-09 16:00:25 +0900

    feat: implement comment submission feature with Firestore integration

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,35 @@
 import { Box, Typography, TextField, Button, Divider } from '@mui/material';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useState } from 'react';
 
 function Home() {
+  const [comment, setComment] = useState('');
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, 'comments'), {
+        // comment: comment,
+        comment,
+        date: serverTimestamp(),
+      });
+      console.log('다음글이 추가되었습니다.: ', docRef.id);
+    } catch (e) {
+      console.error('글 등록시 에러가 발생했습니다', e);
+    }
+  };
+
   return (
     <>
       <Typography variant='h2' component='h2'>
         Home{''}
       </Typography>
-      <Box component='form' sx={{ mt: 2 }}>
+
+      <Box component='form' sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label='Comment'
@@ -16,6 +39,7 @@ function Home() {
           variant='outlined'
           multiline
           rows={5}
+          onChange={handleChange}
         />
         <Button sx={{ mt: 2 }} type='submit' variant='contained'>
           글쓰기

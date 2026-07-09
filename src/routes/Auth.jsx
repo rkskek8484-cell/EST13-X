acd235d87a9117e7c15690c160664a7834576fa7
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-09 11:33:11 +0900

    refactor: implement form handling for user authentication in Auth component

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Box, Typography, TextField, Button } from '@mui/material';
+import { authService } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,7 +9,8 @@ function Auth() {
     email: '',
     password: '',
   });
-  console.log(form);
+
+  const auth = authService;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,13 +20,33 @@ function Auth() {
     }));
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      //로그인
+    }
+  };
+
   return (
     <>
       <Typography variant='h2' component='h2'>
-        Login Form
+        {newAccount ? '회원가입 폼' : '로그인 폼'}
       </Typography>
 
-      <Box component='form' sx={{ mt: 2 }}>
+      <Box component='form' sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label='Email address'
@@ -42,7 +65,7 @@ function Auth() {
           onChange={handleChange}
         />
         <Button sx={{ mt: 2 }} type='submit' variant='contained'>
-          Login
+          {newAccount ? '회원가입' : '로그인'}
         </Button>
       </Box>
     </>

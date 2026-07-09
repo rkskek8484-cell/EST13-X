f3a74db9b31297df8011f674a8c28fed6a246e6f
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-09 14:18:03 +0900

    feat: add navigation component and implement routing for authenticated users

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,29 @@
+import { signOut } from 'firebase/auth';
+import { authService } from '../firebase';
+import { Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router';
+
 function Profile() {
+  const auth = authService;
+  const navigate = useNavigate();
+
+  const onLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/');
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <>
       <h1>Profile</h1>
+
+      <Button sx={{ mt: 2 }} type='submit' variant='contained' onClick={onLogOut}>
+        로그아웃
+      </Button>
     </>
   );
 }

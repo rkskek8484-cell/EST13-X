00421d6bfdca01b6f79457f2a5ef9b8a4bf893ca
rkskek8484-cell <rkskek8484@gmail.com>
2026-07-15 13:49:02 +0900

    feat: 프로필 페이지에서 글 목록 출력

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,11 +1,31 @@
 import { signOut } from 'firebase/auth';
-import { authService } from '../firebase';
-import { Button, Typography } from '@mui/material';
+import { db, authService } from '../firebase';
+import { Button, Typography, Divider, List } from '@mui/material';
 import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { collection, query, where, getDocs, orderBy, onSnapshot } from 'firebase/firestore';
+import Comment from '../components/Comment';
 
 function Profile() {
   const auth = authService;
+  const userId = auth.currentUser.uid;
+
   const navigate = useNavigate();
+  const [comments, setComments] = useState([]);
+
+  const getConmments = async () => {
+    const q = query(collection(db, 'comments'), where('uid', '==', userId), orderBy('date', 'desc'));
+
+    onSnapshot(q, (querySnapshot) => {
+      const commentsArray = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+      setComments(commentsArray);
+    });
+  };
+
+  useEffect(() => {
+    getConmments();
+  }, []);
 
   const onLogOut = () => {
     signOut(auth)
@@ -16,7 +36,7 @@ function Profile() {
         // An error happened.
       });
   };
-
+  console.log(comments);
   return (
     <>
       <h1>Profile</h1>
@@ -24,6 +44,12 @@ function Profile() {
       <Button sx={{ mt: 2 }} type='submit' variant='contained' onClick={onLogOut}>
         로그아웃
       </Button>
+      <Divider sx={{ my: 3 }} />
+      <List sx={{ width: '100%' }}>
+        {comments.map((item) => (
+          <Comment key={item.id} item={item} isShown={userId === item.uid} />
+        ))}
+      </List>
     </>
   );
 }
